06ae7dc7576bb7344df640145e4206f7fd16bd67
Garrett Stein-Seroussi <63983214+gsteinseroussi@users.noreply.github.com>
2020-09-10 11:56:41 -0400

    Add html routes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Workout = require("./models");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +14,18 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", {
   useNewUrlParser: true,
 });
+//html routes
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/exercise", (req, res) => {
+res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+app.get("/stats", (req, res) => {
+res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
 
+//api routes
 app.post("/submit", ({ body }, res) => {
   Workout.create(body)
     .then((dbWorkout) => {
